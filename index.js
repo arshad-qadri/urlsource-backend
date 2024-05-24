bcf2c1ba587beafb8a56c919bbb1d9ba11c36ed5
Arshad Qadri <arshad.qadri@caprium.com>
2024-05-24 11:21:53 +0530

    fixing cors

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ db.once("open", () => {
 });
 
 app.use(express.json());
-app.use("/api", routes);
 app.use(cors())
+app.use("/api", routes);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
